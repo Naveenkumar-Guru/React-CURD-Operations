0f2d7c28a3b0f83bc40a87ca89d0262da31163c1
Naveenkumar506 <naveennani0603@gmail.com>
2025-08-20 16:11:36 +0530

    Table data API

--- src/ReactAPI/Table1API.jsx
+++ src/ReactAPI/Table1API.jsx
@@ -1,23 +1,19 @@
-import axios from "axios";
+import Axios from "axios";
 
 const Table1API = () => {
-  let tablefunction = () => {
-    let getdata = () => {
-      axios
-        .get("https://jsonplaceholder.typicode.com/users")
-        .then((respons) => {
-          console.log(respons.data);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    };
+  let getUserData = () => {
+    Axios.get("https://jsonplaceholder.typicode.com/users")
+      .then((Req) => {
+        console.log(Req.data);
+      })
+      .catch((Error) => {
+        console.log(Error.msg);
+      });
   };
   return (
     <div>
-      <h1>Table API components</h1>
-      <pre>{JSON.stringify.}</pre>
-      <button onClick={tablefunction}>Table data</button>
+      <h1>User components</h1>
+      <button onClick={getUserData}>Get users</button>
     </div>
   );
 };
