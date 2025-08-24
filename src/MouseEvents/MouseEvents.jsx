a6c59e19975c1713e470c5271840eeb3b02cf6ea
Naveenkumar506 <naveennani0603@gmail.com>
2025-08-24 18:20:12 +0530

    Mouse events demo

--- src/MouseEvents/MouseEvents.jsx
+++ src/MouseEvents/MouseEvents.jsx
@@ -1,14 +1,46 @@
 import React from "react";
-
+import "./MouseEvents.css";
 const MouseEvents = () => {
   const handleClick = (e) => {
-    e.target.textContent = "Clicked!.....";
+    e.target.classList.add("onclick-btn"); // apply styles on click
+    e.target.textContent = "Clicked!....."; // change button text
   };
-
+  const DoubleClick = (e) => {
+    e.target.textContent = "Double Clicked!....";
+  };
+  const handleMouseOver = (e) => {
+    e.target.textContent = " Mouse Over!";
+  };
+  const onMouseOut = (e) => {
+    e.target.textContent = "Hover Over Me Again";
+  };
+  let Mousemove = (e) => {
+    alert("Mouse Moving!");
+  };
   return (
-    <div>
-      <button onClick={handleClick}>Click me</button>
-      {""}
+    <div style={{ padding: "20px",margin:"150px" }}>
+      <h2>Mouse Events Demo</h2>
+
+      <button className="onclick-btn" onClick={handleClick}>
+        Click me
+      </button>
+      <br></br>
+      <br></br>
+      <button className="DoubleClick-btn" onDoubleClick={DoubleClick}>
+        DoubleClick
+      </button>
+      <br></br>
+      <br></br>
+      <button
+        className="MouseOver-btn"
+        onMouseOver={handleMouseOver}
+        onMouseOut={onMouseOut}
+      >
+        Hover Over Me
+      </button>
+      <br></br>
+      <br></br>
+      <button onMouseMove={Mousemove}>Move Mouse Here</button>
     </div>
   );
 };
