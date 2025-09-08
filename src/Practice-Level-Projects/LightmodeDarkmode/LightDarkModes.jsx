4b8f7d9c95ca046ffe7f7e584e1dd937537e3137
Naveenkumar506 <naveennani0603@gmail.com>
2025-09-08 22:03:33 +0530

    class component

--- src/Practice-Level-Projects/LightmodeDarkmode/LightDarkModes.jsx
+++ src/Practice-Level-Projects/LightmodeDarkmode/LightDarkModes.jsx
@@ -1,35 +1,54 @@
 // import React from "react";
+// import "./LightDarkmodes.css";
+
 // import { useState } from "react";
-
 // const LightDarkModes = () => {
-//   const [darkMode, setDarkMode] = useState(false);
+//   const [darkMode, setdarkmode] = useState(false);
 
+//   const handle = () => {
+//     setdarkmode(!darkMode);
+//   };
 //   return (
 //     <div className={darkMode ? "dark" : "light"}>
-//       <h1>{darkMode ? "🌙 Dark Mode" : "☀️ Light Mode"}</h1>
-//       <button onClick={() => setDarkMode(!darkMode)}>Toggle Theme</button>
+//       <h1>{darkMode ? "Dark Mode" : "Light Mode"}</h1>
+//       <button onClick={handle}>Toggle them</button>
 //     </div>
 //   );
 // };
 
 // export default LightDarkModes;
 
-import React from "react";
+//class component
+import React, { Component } from "react";
 import "./LightDarkmodes.css";
 
-import { useState } from "react";
-const LightDarkModes = () => {
-  const [darkMode, setdarkmode] = useState(false);
+class LightDarkModes extends Component {
+  constructor(props) {
+    super(props);
 
-  const handle = () => {
-    setdarkmode(!darkMode);
+    // state in class component
+    this.state = {
+      darkMode: false,
+    };
+  }
+
+  // method to toggle
+  handleToggle = () => {
+    this.setState((prevState) => ({
+      darkMode: !prevState.darkMode,
+    }));
   };
-  return (
-    <div className={darkMode ? "dark" : "light"}>
-      <h1>{darkMode ? "Dark Mode" : "Light Mode"}</h1>
-      <button onClick={handle}>Toggle them</button>
-    </div>
-  );
-};
+
+  render() {
+    const { darkMode } = this.state;
+
+    return (
+      <div className={darkMode ? "dark" : "light"}>
+        <h1>{darkMode ? "Dark Mode" : "Light Mode"}</h1>
+        <button onClick={this.handleToggle}>Toggle Theme</button>
+      </div>
+    );
+  }
+}
 
 export default LightDarkModes;
